39c59f19eba53fc6b3df1eaa586af6999d046d86
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-02 22:27:31 +0530

    Create game function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function computePlay() {
     let num = Math.floor(Math.random() * 3);
-    const computer = ['Rock','Paper','Scissors'];
+    const computer = ['Rock', 'Paper', 'Scissors'];
     return computer[num];
+}
+function game() {
+
 }
