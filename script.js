60dbe5f34bf436c7e27f52a00a5b4f26bcedda97
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-17 20:18:19 +0530

    add: format all code using prettier

--- script.js
+++ script.js
@@ -3,48 +3,47 @@ let computerScore = 0;
 let round = 1;
 
 const computerChoice = () => {
-    const computer = ['rock', 'paper', 'scissors'];
-    return computer[Math.floor(Math.random() * 3)];
+  const computer = ['rock', 'paper', 'scissors'];
+  return computer[Math.floor(Math.random() * 3)];
 };
 
 const playRound = (e) => {
-    let playerSelection = e.target.classList.value;
-    let computerSelection = computerChoice();
+  let playerSelection = e.target.classList.value;
+  let computerSelection = computerChoice();
 
-    if (round >= 6) return;
+  if (round >= 6) return;
 
-    document.querySelector(
-        `.round .round${round}`
-    ).textContent = `${round} - You: ${playerSelection.toUpperCase()} Computer: ${computerSelection.toUpperCase()}`;
+  document.querySelector(
+    `.round .round${round}`
+  ).textContent = `${round} - You: ${playerSelection.toUpperCase()} Computer: ${computerSelection.toUpperCase()}`;
 
-    round++;
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'scissors') playerScore++;
-        else computerScore++;
-    } else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') playerScore++;
-        else computerScore++;
-    } else if (playerSelection === 'scissors') {
-        if (computerSelection === 'paper') playerScore++;
-        else computerScore++;
-    }
+  round++;
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'scissors') playerScore++;
+    else computerScore++;
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') playerScore++;
+    else computerScore++;
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'paper') playerScore++;
+    else computerScore++;
+  }
 
-    updateScores(playerScore, computerScore);
+  updateScores(playerScore, computerScore);
 };
 
 const updateScores = (playerScore, computerScore) => {
-    const scores = document.querySelector('.score');
-    const winnerBoard = document.querySelector('.winner-board');
-    if (round > 5) {
-        if (playerScore > computerScore) winnerBoard.textContent = `YOU WON!`;
-        else if (computerScore > playerScore)
-            winnerBoard.textContent = `COMPUTER WON`;
-        else winnerBoard.textContent = "It's a tie";
-    }
-    scores.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+  const scores = document.querySelector('.score');
+  const winnerBoard = document.querySelector('.winner-board');
+  if (round > 5) {
+    if (playerScore > computerScore) winnerBoard.textContent = `YOU WON!`;
+    else if (computerScore > playerScore) winnerBoard.textContent = `COMPUTER WON`;
+    else winnerBoard.textContent = "It's a tie";
+  }
+  scores.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
 };
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', playRound);
+  button.addEventListener('click', playRound);
 });
